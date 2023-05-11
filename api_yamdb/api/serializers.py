9f6c299042ad155c9818e732957e856e101fdc59
Vadim <vadim.popov7277@yandex.ru>
2023-05-12 00:38:12 +0500

    Auth/Users

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from api.permissions import IsAdminOrStaff
+from users.models import User
+
+USERNAME_CHECK = r'^[\w.@+-]+$'  # Проверка имени на отсутствие спецсимволов
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email', 'username')
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    username = serializers.RegexField(
+        regex=USERNAME_CHECK,
+        max_length=150,
+        required=True
+    )
+    confirmation_code = serializers.CharField(
+        required=True,
+        max_length=16,
+    )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    permission_classes = (IsAdminOrStaff,)
+
+    class Meta:
+        model = User
+        fields = (
+            'username', 'email', 'first_name',
+            'last_name', 'bio', 'role'
+        )
